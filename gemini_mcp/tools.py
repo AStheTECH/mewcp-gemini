f4e06b90c3ef89d6a643305ce0f63f71d0c42ecb
Rohit Gite <rohitgite1419@gmail.com>
2026-05-05 17:47:23 +0530

    updated with fastmcp-credentials

--- gemini_mcp/tools.py
+++ gemini_mcp/tools.py
@@ -1,6 +1,7 @@
 import requests
 from fastmcp import FastMCP
 from fastmcp.exceptions import ToolError
+from fastmcp_credentials import get_credentials
 from pydantic import Field
 from .config import GEMINI_BASE_URL
 from .schemas import GeminiGenerateTextResponse
@@ -11,15 +12,15 @@ def register_tools(mcp: FastMCP) -> None:
     @mcp.tool("gemini_ai_generate_text", description="Generate text using Gemini LLM")
     def gemini_ai_generate_text(
         query: str = Field(..., description="Natural language prompt to send to Gemini."),
-        gemini_api_key: str = Field(..., description="Google Gemini API key."),
-        model: Optional[str] = Field(default= "gemini-2.5-flash", description="Gemini model name, e.g., 'gemini-2.5-flash' or 'gemini-2.5-pro'."),
+        model: Optional[str] = Field(default="gemini-2.5-flash", description="Gemini model name, e.g., 'gemini-2.5-flash' or 'gemini-2.5-pro'."),
     ) -> GeminiGenerateTextResponse:
         """
         Returns:
             A dictionary containing the original prompt and generated text response.
         """
+        creds = get_credentials()
         headers = {"Content-Type": "application/json"}
-        params = {"key": gemini_api_key}
+        params = {"key": creds.api_key}
 
         body = {"contents": [{"parts": [{"text": query}]}]}
 
